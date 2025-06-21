5f8d0a8e203426d9320eea01ab605364d048c8ce
nahidn228 <nahidn228@gmail.com>
2025-06-21 22:48:09 +0600

    server connected to DB using Mongoose

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import config from "./config";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(config.port, () => {
+  console.log(` server is running on port ${config.port}`);
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    success: true,
+    message: "BookNest is Running",
+  });
+});
+
+async function server() {
+  try {
+    await mongoose.connect(config.database_env!);
+    console.log(`✅ Connected to mongoDB`);
+  } catch (error: any) {
+    console.log(`server error: ${error.message}`);
+  }
+}
+server();
